9a87e8054f52b6da4ee24bc4e02b118abae07c5b
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2021-10-25 08:44:02 -0600

    Add files via upload

---
+++ spellcheck.py
@@ -0,0 +1,32 @@
+# Spell Check Starter
+# This start code creates two lists
+# 1: dictionary: a list containing all of the words from "dictionary.txt"
+# 2: aliceWords: a list containing all of the words from "AliceInWonderland.txt"
+
+import re  # Needed for splitting text with a regular expression
+
+
+def main():
+    # Load data files into lists
+    dictionary = loadWordsFromFile("data-files/dictionary.txt")
+    aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
+
+    # Print first 50 values of each list to verify contents
+    print(dictionary[0:50])
+    print(aliceWords[0:50])
+# end main()
+
+
+def loadWordsFromFile(fileName):
+    # Read file as a string
+    fileref = open(fileName, "r")
+    textData = fileref.read()
+    fileref.close()
+
+    # Split text by one or more whitespace characters
+    return re.split('\s+', textData)
+# end loadWordsFromFile()
+
+
+# Call main() to begin program
+main()
